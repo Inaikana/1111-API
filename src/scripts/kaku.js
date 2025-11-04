6c616401cd32393fdaaab797295301a200e00adc
kitty <Kitty@kitty.com>
2025-11-04 15:52:05 +0800

    做出指定顯示什麼頁面 跟 判斷顯示什麼頁面的函數

--- src/scripts/kaku.js
+++ src/scripts/kaku.js
@@ -1,7 +1,27 @@
 function henko() {
   return {
     // 回傳一個物件
-    mode: 1, // 在物件裡是【 key : value , 】
+    mode: "ikou", // 在物件裡是【 key : value , 】
+
+    ikou: function () {
+      this.mode = "ikou"
+    },
+    tsukuru: function () {
+      // 作る
+      this.mode = "tsukuru"
+    },
+    sagasu: function () {
+      this.mode = "sagasu"
+    },
+    ikouAru: function () {
+      return this.mode == "ikou" //讓它回傳一個布林值 true或false
+    },
+    tsukuruAru: function () {
+      return this.mode == "tsukuru"
+    },
+    sagasuAru: function () {
+      return this.mode == "sagasu"
+    },
   }
 }
 
